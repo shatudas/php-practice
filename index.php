14931ccb860ca3c88a141e24f82262101b29980d
shatudas <das.shatu2000@gmail.com>
2023-04-10 09:27:40 +0600

    php work

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Syntex</title>
+</head>
+<body>
+
+	<h2>PHP Syntex</h2>
+	<br>
+	<hr>
+
+	<?php
+
+	echo "<br>";
+	$a = 1;
+	ECHO "$a";
+
+	?>
+
+
+
+</body>
+</html>
